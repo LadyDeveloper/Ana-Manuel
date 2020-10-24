3b6103c8a6f7b875d134dd4c55a18f93985437d8
LadyDeveloper <anhacorrea@hotmail.com>
2020-10-23 21:57:41 -0700

    Styles added

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
- const changeTitle = setInterval( () => {
-    document.querySelector('h1').innerHTML = document.querySelector('h1').innerHTML == "Lady Developer" ? "Ana Manuel" : "Lady Developer" ;
- }, 1000);
+const changeTitle = setInterval(() => {
+  document.querySelector("h1").innerHTML =
+    document.querySelector("h1").innerHTML == "Lady Developer"
+      ? "Ana Manuel"
+      : "Lady Developer";
+      
+    if (document.querySelector("h1").innerHTML === "Lady Developer") {
+        document.querySelector("h1").style.color = " #146EB0";
+    } else {
+        document.querySelector("h1").style.color = "#408b35";
+    }
+}, 1500);
 
- const changeBgColor = () => {
-     let bgColor = document.body;
-     bgColor.style.backgroundColor = bgColor.style.backgroundColor === 'black' ? 'white' : 'black';
-     bgColor.style.color = bgColor.style.color === 'white' ? 'black' : 'white';
- };
 
+const changeBgColor = () => {
+  let bgColor = document.body;
+  bgColor.style.backgroundColor =
+    bgColor.style.backgroundColor === "black" ? "white" : "black";
+  bgColor.style.color = bgColor.style.color === "white" ? "black" : "white";
+};
