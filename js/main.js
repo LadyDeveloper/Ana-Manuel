4f4e124101b70cea5e3e7fb4624db0684abb6a9b
LadyDeveloper <anhacorrea@hotmail.com>
2020-11-11 12:06:54 -0800

    Responsive design

--- js/main.js
+++ js/main.js
@@ -3,18 +3,14 @@ const changeTitle = setInterval(() => {
     document.querySelector("span").innerHTML == "a Developer"
       ? "Ana"
       : "a Developer";
-      
-    if (document.querySelector("span").innerHTML === "a Developer") {
-        document.querySelector("span").style.color = " black";
-    } else {
-        document.querySelector("span").style.color = "#ffff";
-    }
+
+  if (document.querySelector("span").innerHTML === "a Developer") {
+    document.querySelector("span").style.color = " black";
+  } else {
+    document.querySelector("span").style.color = "#ffff";
+  }
 }, 1500);
 
-
-const changeBgColor = () => {
-  let bgColor = document.body;
-  bgColor.style.backgroundColor =
-    bgColor.style.backgroundColor === "black" ? "white" : "black";
-  bgColor.style.color = bgColor.style.color === "white" ? "black" : "white";
+const showMenu = () => {
+  console.log("Clicked");
 };
