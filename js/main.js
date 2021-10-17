5705589ca886c1889205c6f00cda31e90c69f65c
ladydeveloper <anhacorrea@hotmail.com>
2021-10-16 19:38:52 -0700

    Fixed layout

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
 const changeTitle = setInterval(() => {
   document.querySelector("span").innerHTML =
-    document.querySelector("span").innerHTML == "a Developer"
-      ? "Ana"
-      : "a Developer";
+    document.querySelector("span").innerHTML == "a Software Developer"
+      ? "an Anime Enthusiast"
+      : "a Software Developer";
 
-  if (document.querySelector("span").innerHTML === "a Developer") {
-    document.querySelector("span").style.color = " black";
+  if (document.querySelector("span").innerHTML === "a Software Developer") {
+    document.querySelector("span").style.color = "#8269dd";
   } else {
-    document.querySelector("span").style.color = "#ffff";
+    document.querySelector("span").style.color = "#e04b95";
   }
 }, 1500);
 
 const showMenu = () => {
   console.log("Clicked");
 };
+
+document.getElementById('year').innerHTML = new Date().getFullYear();
